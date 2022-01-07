81236bcebc1c7368992f124703a0cf27017952a6
Chugaister <dmytroshvahyliak@gmail.com>
2022-01-07 20:42:11 +0200

    smart contract test

--- contract.sol
+++ contract.sol
@@ -1,13 +1,29 @@
-pragma solidity ^0.8.7;
+pragma solidity 0.8.2;
+
+//import "https://github.com/pancakeswap/pancake-swap-periphery/blob/master/contracts/interfaces/IPancakeRouter01.sol";
+//import "https://github.com/binance-chain/bsc-genesis-contract/blob/master/contracts/interface/IBEP20.sol";
+
+interface IPancakeRouter01 {
+   function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+}
+
+interface IBEP20 {
+   function balanceOf(address account) external view returns (uint256);
+}
 
 contract germesContract {
 
    address owner;
    address[] users;
+   address WBNB;
 
    constructor() {
       owner = msg.sender;
       users.push(owner);
+      WBNB = 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd;
    }
 
    function isUser(address _address) public view returns (bool) {
@@ -20,17 +36,17 @@ contract germesContract {
    }
 
    function transferOwnership(address newOwner) external {
-      require(msg.sender == owner, "GERMES: ACCESS_DENIED");
+      require(msg.sender == owner, "Germes: ACCESS_DENIED");
       owner = newOwner;
    }
 
    function addUser(address _address) external {
-      require(msg.sender == owner, "GERMES: ACCESS_DENIED");
+      require(msg.sender == owner, "Germes: ACCESS_DENIED");
       users.push(_address);
    }
 
    function delUser(address _address) external {
-      require(msg.sender == owner, "GERMES: ACCESS_DENIED");
+      require(msg.sender == owner, "Germes: ACCESS_DENIED");
       for (uint i = 0; i < users.length; ++i) {
          if (users[i] == _address) {
             delete users[i];     
@@ -38,13 +54,32 @@ contract germesContract {
       }
    }
 
-   function multiswap(uint256 amountIn, address[] calldata path) external {
-      require(isUser(msg.sender), "GERMES: ACCESS_DENIED");
-      for (uint i = 0; i < path.length; ++i) {
-         address token1 = path[i];
-         address token2 = path[i+1];
-         //swap();
+   /*function multiswap(uint256 amountIn, address[] calldata path) external payable {
+      require(isUser(msg.sender), "Germes: ACCESS_DENIED");
+      if (path[0] == WBNB) {
+         
+      } else {
+         IBEP20(path[0]).transferFrom(msg.sender, address(this), amountIn);
       }
+      for (uint i = 0; i < path.length; ++i) {
+         address token0 = path[i];
+         address token1 = path[i+1];
+         address[2] memory ticker = [token0, token1];
+
+         //swap(0, );
+      }
+   }*/
+
+   function swapBNBForToken(address token1) external payable {
+      address[] memory path = new address[](2);
+      path[0] = WBNB;
+      path[1] = token1;
+      IPancakeRouter01(0xD99D1c33F9fC3444f8101754aBC46c52416550D1).swapExactETHForTokens{value: msg.value}(
+         0,
+         path,
+         msg.sender,
+         block.timestamp + 100
+      );
    }
 
    function getOwner() external view returns (address) {
@@ -55,15 +90,17 @@ contract germesContract {
       return users;
    }
 
-   function balance() external view returns(uint256) {
-      return address(this).balance;
+   function balanceOf(address token) external view returns(uint256) {
+      if (token == WBNB) {
+         return address(this).balance;
+      } else {
+         return IBEP20(token).balanceOf(address(this));
+      }
    }
 
    function destroy(address payable to) public {
-      require(msg.sender == owner, "GERMES: ACCESS_DENIED");
+      require(msg.sender == owner, "Germes: ACCESS_DENIED");
       selfdestruct(to);
    }
-
-   function recieve() external payable {}
 
 }
--- test.py
+++ test.py
@@ -1,61 +1,24 @@
-import config, dbms, time, manBlockchainFuncs
-from worker import Worker
 from web3 import Web3
-from colorama import init
-init()
-from colorama import Fore, Back, Style
+import config
+import json
+web3 = Web3(Web3.HTTPProvider(config.BSCURL))
+print("Blockchain node connection:", web3.isConnected())
 
-'''
-def viewPrices():
-	for symbol in w.supportedTokens():
-		for exchange in w.supportedExchanges():
-			try:
-				ticker = symbol+"/WBNB"
-				if w.pairAvailable(ticker, exchange):
-					print(Fore.GREEN+ exchange, ticker, w.price(ticker, 1, exchange))
-				else:
-					print(Fore.YELLOW+f"{ticker} pair is unavailable on {exchange}")
-			except:
-				print(Fore.RED+ "FATAL ERROR")
-'''
+with open("data/customABI.json", "r") as file:
+	contractABI = json.load(file)
+address = "0xfA403A78aC03084b89473ddD57c952CDdBc69C72"
+contract = web3.eth.contract(address = address, abi = contractABI)
 
-#print(f'{w.priceImpact("Cake/WBNB", 1.01, "BabySwap")*100}%')
-'''
-def figurePriceImpact():
-	import matplotlib.pyplot as plt
-	import numpy as np
+txn = contract.functions.swapBNBForToken(web3.toChecksumAddress("0xc98d826bc611ec1c54ec1674ed4bfce1aa9a5ddf")).buildTransaction(
+			{
+				'from': config.wallet3,
+				'value': 1000000,
+				'gasLimit': 50000000,
+				'gasPrice': web3.toWei(10, "gwei"),
+				'nonce': web3.eth.get_transaction_count(config.wallet3)
+				}
+			)
 
-	#x = np.linspace(1, 1000, 1000)
-	#y = w.priceImpact("WBNB/BUSD", x, "PancakeSwap (v2)")
-
-	x = [i for i in range(1, 500)]
-	y = []
-	for amountIn in x:
-		y.append(w.priceImpact("WINGS/BUSD", amountIn, "Jetswap")*100)
-	fig, ax = plt.subplots()
-	ax.plot(x, y)
-	plt.show()
-figurePriceImpact()
-
-for exchangeName in dbms.getExchangeNames():
-	router = dbms.getRouterAddress(exchangeName)
-	ABI = dbms.getRouterABI(exchangeName)
-	pancakeContract = manBlockchainFuncs.web3.eth.contract(address = router, abi = ABI)
-
-	factoryAddress = pancakeContract.functions.factory().call()
-	factoryABI = dbms.getFactoryABI(exchangeName)
-	factoryContract = manBlockchainFuncs.web3.eth.contract(address = factoryAddress, abi = factoryABI)
-
-	pairAddress = factoryContract.functions.getPair(
-		manBlockchainFuncs.web3.toChecksumAddress(dbms.getTokenAddress("WBNB")),
-		manBlockchainFuncs.web3.toChecksumAddress(dbms.getTokenAddress("BUSD"))).call()
-	print(f"{exchangeName}: \n\tpair address: {pairAddress} \n\tURL for contract pair ABI: https://bscscan.com/address/{pairAddress}#code")
-	
-'''
-
-from worker import Worker
-	
-trader = Worker(config.wallet, config.key)
-
-print(trader.customBalance("0x64fcEBf1bb8F43FB19A971f687F83A1d9C1C1f11")*(10**15))
-
+signedTxn = web3.eth.account.sign_transaction(txn, config.key3)
+txToken = web3.eth.send_raw_transaction(signedTxn.rawTransaction)
+print(web3.toHex(txToken))
